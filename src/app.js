b4d0ab531ba07f9799f7daa45e175e0e0cc6f5f8
Jatin <jatinanshul08@gmail.com>
2026-03-24 00:42:24 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res) => {
+    res.send({FirstName:"Jatin" ,lastname:"Kumar"}) //request handler
+});
+
+app.post("/user",(req,res) => {
+    res.send("data saved to the database successfullu") //request handler
+});
+
+app.delete("/user",(req,res) => {
+    res.send("Deleted successfully") //request handler
+});
 app.use("/test",(req,res) => {
     res.send("Hello from the jack") //request handler
 });
